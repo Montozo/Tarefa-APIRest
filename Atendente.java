f512091ec64f0b2535cf4fdcd98ecddb1bd8a706
Kaiky Montozo Costa <montozocosta@gmail.com>
2025-12-03 13:59:19 -0300

    Delete Atendente.java

--- Atendente.java
+++
@@ -1,9 +0,0 @@
-import java.math.BigDecimal;
-
-public class Atendente extends Empregados{
-    @Override
-    public BigDecimal calcularBonificacao(BigDecimal salario) {
-        BigDecimal bonificacao = salario.multiply(BigDecimal.valueOf(0.3));
-        return bonificacao;
-    }
-}
