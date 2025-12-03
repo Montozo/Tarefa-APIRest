30ba657d34aca18e77ef34b61e9311e1040157e7
Kaiky Montozo Costa <montozocosta@gmail.com>
2025-12-03 14:00:57 -0300

    Delete Zelador.java

--- Zelador.java
+++
@@ -1,9 +0,0 @@
-import java.math.BigDecimal;
-
-public class Zelador extends Empregados{
-    @Override
-    public BigDecimal calcularBonificacao(BigDecimal salario) {
-        BigDecimal bonificacao = salario.multiply(BigDecimal.valueOf(0.1));
-        return bonificacao;
-    }
-}
